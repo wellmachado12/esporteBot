6d552550a00b8fa3917722d57355fec77e91dca6
TechGui <guilhermedrsilva7@gmail.com>
2025-07-08 13:41:35 -0300

    Implementação do padrão Facade e migração do banco de dados

--- meu-app/migrations/20250707180602_add_conversation_id_to_conversations.js
+++ meu-app/migrations/20250707180602_add_conversation_id_to_conversations.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.alterTable("conversations", function (table) {
     table.integer("conversation_id").unsigned().notNullable().defaultTo(1);
     table.index("conversation_id");
@@ -13,7 +13,7 @@ exports.up = function(knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.alterTable("conversations", function (table) {
     table.dropIndex("conversation_id");
     table.dropColumn("conversation_id");
